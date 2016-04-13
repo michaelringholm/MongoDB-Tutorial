43b34dda1cd1533e697521cf072bf6b142d1b818
michaelringholm <chssdm@audatex.com>
2016-04-13 16:00:49 +0200

    Latest changes

--- src/dk/opusmagus/db/mongo/dac/MongoDBDAC.java
+++ src/dk/opusmagus/db/mongo/dac/MongoDBDAC.java
@@ -3,10 +3,16 @@ package dk.opusmagus.db.mongo.dac;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
+import java.util.regex.Pattern;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.Block;
+import com.mongodb.CommandResult;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.QueryBuilder;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
@@ -27,19 +33,23 @@ public class MongoDBDAC {
 	}
 	
 	public static void main(String[] args) {
-		System.out.println("MongoDB tutorial started...");				
+		System.out.println("MongoDB tutorial started...");
+		Date start = new Date();
 		
 		MongoDBDAC dac = new MongoDBDAC();
 		
 		try {
-			dac.createMassData();
-			dac.getDocuments("trades");
+			//dac.createMassData();
+			//dac.getDocuments("trades");
+			//dac.getDocumentsByTopLevelFilter("trades");
+			dac.getDocumentsByEmbeddedFilter("trades");
 		} 
 		catch (Exception e) {
 			e.printStackTrace();
 		}
 		
-		System.out.println("MongoDB tutorial ended!");
+		Date end = new Date();
+		System.out.println("MongoDB tutorial ended in " + String.valueOf(end.getTime()-start.getTime()) + " ms!");
 	}
 	
 	private void getDocuments(String collectionName)
@@ -58,7 +68,7 @@ public class MongoDBDAC {
 	{
 		//MongoCollection<Document> iterable = db.getCollection(collectionName).find(eq("borough", "Manhattan"));
 		FindIterable<Document> iterable = db.getCollection(collectionName).find(
-		        new Document("tradeType", "FX Option"));
+		        new Document("_id", "565432cb8d1c773e4430ade3"));
 		
 		iterable.forEach(new Block<Document>() {
 		    @Override
@@ -67,10 +77,62 @@ public class MongoDBDAC {
 		    }
 		});
 	}
+	
+	private void getDocumentsByEmbeddedFilter(String collectionName)
+	{
+		//MongoCollection<Document> iterable = db.getCollection(collectionName).find(eq("borough", "Manhattan"));
+		FindIterable<Document> iterable = db.getCollection(collectionName).find(
+		        new Document("trade.tradeType", "Floor"));
+		//FindIterable<Document> iterable = db.getCollection(collectionName).find(
+		        //new Document("tradeType", "Swap"));
+		
+		iterable.forEach(new Block<Document>() {
+		    @Override
+		    public void apply(final Document document) {
+		        System.out.println(document);
+		    }
+		});
+	}
+	
+	private void getDocumentsByFuzzyFilter(String collectionName)
+	{
+		//DBObject q = QueryBuilder.start("trade.tradeType").is(Pattern.compile("oo", 
+          //      Pattern.CASE_INSENSITIVE)).get();
+		MongoCollection<Document> coll = db.getCollection(collectionName);
+		//coll.find(new Document("trade.tradeType", "Floor"));
+		
+		//MongoCollection<Document> iterable = db.getCollection(collectionName).find(eq("borough", "Manhattan"));
+		//FindIterable<Document> iterable = db.getCollection(collectionName).find(
+		  //      new Document("trade.tradeType", "Floor"));
+		//FindIterable<Document> iterable = db.getCollection(collectionName).find(
+		        //new Document("tradeType", "Swap"));
+		
+	    /*DBObject textSearchCommand = new BasicDBObject();
+	    textSearchCommand.put("text", collectionName);
+	    textSearchCommand.put("search", "oo");
+	    CommandResult commandResult = db.runCommand(textSearchCommand);
+		
+		coll.forEach(new Block<Document>() {
+		    @Override
+		    public void apply(final Document document) {
+		        System.out.println(document);
+		    }
+		});*/
+	}
 
 	private void createMassData() throws Exception {
-		for(int i=0; i<500; i++)
+		for(int i=0; i<5; i++)
 			insertDocument("trades");
+	}
+	
+	private void dropCollection(String collectionName)
+	{
+		db.getCollection(collectionName).drop();
+	}
+	
+	private void createIndex(String collectionName)
+	{
+		db.getCollection(collectionName).createIndex(new Document("trade.tradeType", 1));
 	}
 
 	private void insertDocument(String collectionName) throws Exception {
@@ -78,14 +140,14 @@ public class MongoDBDAC {
 		 * the driver automatically adds the field to the document and sets the field’s value 
 		 * to a generated ObjectId.	 */
 		
-		DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH);
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
 		db.getCollection(collectionName).insertOne(
 	        new Document("trade",
                 new Document()
 	        	.append("accountNo", "11232-333-23123-33")
-                .append("tradeType", "FX Option")
-                .append("tradeAmount", "5200000")
-                .append("tradeDate", format.parse("2014-10-01T00:00:00Z"))
+                .append("tradeType", "Floor")
+                .append("tradeAmount", new Date().getTime())
+                .append("tradeDate", format.format(new Date()))
                 ));
                 /*.append("tradeMetaData", asList(
                         new Document()
